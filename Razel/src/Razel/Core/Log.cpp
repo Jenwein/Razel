e03ca387dfd711f99245e7610e9f7b1181a4a4bf
Jenwein <rgw127310@gmail.com>
2025-04-08 14:16:23 +0800

    [ADD] 添加日志输出到文本

--- Razel/src/Razel/Core/Log.cpp
+++ Razel/src/Razel/Core/Log.cpp
@@ -3,7 +3,7 @@
 #include "Razel/Core/Log.h"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
-
+#include <spdlog/sinks/basic_file_sink.h>
 namespace Razel
 {
 	// 初始化静态成员
@@ -12,14 +12,22 @@ namespace Razel
 
 	void Log::Init()
 	{
-		// 设置日志模式
-		spdlog::set_pattern("%^[%T] %n: %v%$");
 
-		s_CoreLogger = spdlog::stdout_color_mt("RAZEL");
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());					// 控制台输出
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Razel.log", true));	// 文件输出
+
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("RAZEL", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt("app");
-		s_ClientLogger ->set_level(spdlog::level::trace);
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
+		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
-
 }
