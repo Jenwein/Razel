f3d913e90c8a1b6d02790b6b37327e2a16daba88
Jenwein <rgw127310@gmail.com>
2025-04-20 20:05:08 +0800

    [ADD]添加基本的帧缓冲

---
+++ Razel/src/Razel/Renderer/Framebuffer.cpp
@@ -0,0 +1,22 @@
+#include "rzpch.h"
+#include "Framebuffer.h"
+
+#include "Razel/Renderer/Renderer.h"
+
+#include "Platform/OpenGL/OpenGLFramebuffer.h"
+
+namespace Razel {
+
+	Ref<Framebuffer> Framebuffer::Create(const FramebufferSpecification& spec)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    RZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLFramebuffer>(spec);
+		}
+
+		RZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+}
--- premake5.lua
+++ premake5.lua
@@ -162,3 +162,51 @@ group ""
 			defines "RZ_DIST"
 			runtime "Release"
 			optimize "on"
+
+	project "Razel-Editor"
+		location "Razel-Editor"
+		kind "ConsoleApp"
+		language"C++"
+		cppdialect "C++17"
+		staticruntime "on"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
+		}
+
+		includedirs
+		{
+			"Razel/vendor/spdlog/include",
+			"Razel/src",
+			"Razel/vendor",
+			"%{IncludeDir.glm}"
+
+		}
+
+		links
+		{
+			"Razel"
+		}
+
+		filter "system:windows"
+			systemversion "latest"
+
+		filter "configurations:Debug"
+			defines "RZ_DEBUG"
+			runtime "Debug"
+			symbols "on"
+
+		filter "configurations:Release"
+			defines "RZ_RELEASE"
+			runtime "Release"
+			optimize "on"
+
+		filter "configurations:Dist"
+			defines "RZ_DIST"
+			runtime "Release"
+			optimize "on"
