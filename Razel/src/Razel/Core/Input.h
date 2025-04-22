5ede27dd63d0121325d3a70a37eec5d05500675d
Jenwein <rgw127310@gmail.com>
2025-04-22 17:25:59 +0800

    [ADD]重构Input类

--- Razel/src/Razel/Core/Input.h
+++ Razel/src/Razel/Core/Input.h
@@ -7,31 +7,11 @@ namespace Razel
 {
 	class Input
 	{
-	protected:
-		Input() = default;
 	public:
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
-		virtual ~Input() = default;
-		// 全局范围内访问输入状态
-		static bool IsKeyPressed(KeyCode keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
-
-		static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-		static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-	protected:
-		// 由平台去继承实现具体方法
-		virtual bool IsKeyPressedImpl(KeyCode keycode) = 0;
-		virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-
-		static Scope<Input> Create();
-
-	private:
-		static Scope<Input> s_Instance;
+		static bool IsKeyPressed(KeyCode keycode);
+		static bool IsMouseButtonPressed(MouseCode button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
-
 }
