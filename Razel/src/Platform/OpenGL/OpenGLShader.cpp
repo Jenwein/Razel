09f5d9244054b46cab21fa750762cf03a639f09d
Jenwein <rgw127310@gmail.com>
2025-08-21 14:13:04 +0800

    [MATIN]维护代码 - 扩展添加SetFloat2方法 - 修复VertexArray以支持整数属性 - 修复ImGuizmo,只有在不使用的时候才可以更换type

--- Razel/src/Platform/OpenGL/OpenGLShader.cpp
+++ Razel/src/Platform/OpenGL/OpenGLShader.cpp
@@ -234,6 +234,13 @@ namespace Razel
 		UploadUniformFloat(name, value);
 	}
 
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	{
+		RZ_PROFILE_FUNCTION();
+
+		UploadUniformFloat2(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		RZ_PROFILE_FUNCTION();
