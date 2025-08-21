8965731f23710a88288b988c1099fa85599bd893
Jenwein <rgw127310@gmail.com>
2025-08-21 17:31:39 +0800

    [EDIT]修复TabBar可用时的视口边界计算错误

--- Razel-Editor/src/EditorLayer.cpp
+++ Razel-Editor/src/EditorLayer.cpp
@@ -271,7 +271,13 @@ namespace Razel {
 
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0,0 });
 		ImGui::Begin("Viewport");
-		auto viewportOffset = ImGui::GetCursorPos(); // Includes tab bar
+		
+		auto viewportMinRegion = ImGui::GetWindowContentRegionMin();
+		auto viewportMaxRegion = ImGui::GetWindowContentRegionMax();
+		auto viewportOffset = ImGui::GetWindowPos();
+		m_ViewportBounds[0] = { viewportMinRegion.x + viewportOffset.x, viewportMinRegion.y + viewportOffset.y };
+		m_ViewportBounds[1] = { viewportMaxRegion.x + viewportOffset.x, viewportMaxRegion.y + viewportOffset.y };
+
 		m_ViewportFocused = ImGui::IsWindowFocused();
 		m_ViewportHovered = ImGui::IsWindowHovered();
 		Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused && !m_ViewportHovered);
@@ -283,15 +289,6 @@ namespace Razel {
 		uint64_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
 		ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 		
-		auto windowSize = ImGui::GetWindowSize();
-		ImVec2 minBound = ImGui::GetWindowPos();
-		minBound.x += viewportOffset.x;
-		minBound.y += viewportOffset.y;
-		ImVec2 maxBound = { minBound.x + m_ViewportSize.x, minBound.y + m_ViewportSize.y };
-
-		m_ViewportBounds[0] = { minBound.x, minBound.y };
-		m_ViewportBounds[1] = { maxBound.x, maxBound.y };
-
 		// Gizmos
 		Entity selectedEntity = m_SceneHierarchyPanel.GetSelectedEntity();
 		if (selectedEntity && m_GizmoType != -1)
@@ -303,8 +300,9 @@ namespace Razel {
 			float windowHeight = (float)ImGui::GetWindowHeight();
 
 			// 设置Gizmo视口
-			ImGuizmo::SetRect(ImGui::GetWindowPos().x, ImGui::GetWindowPos().y, windowWidth, windowHeight);
-
+			ImGuizmo::SetRect(m_ViewportBounds[0].x,  m_ViewportBounds[0].y, 
+							  m_ViewportBounds[1].x - m_ViewportBounds[0].x, 
+							  m_ViewportBounds[1].y - m_ViewportBounds[0].y);
 			// 相机
 			//auto cameraEntity = m_ActiveScene->GetPrimaryCameraEntity();
 			//const auto& camera = cameraEntity.GetComponent<CameraComponent>().Camera;
